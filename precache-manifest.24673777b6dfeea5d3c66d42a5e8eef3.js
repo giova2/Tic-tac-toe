11be9707c9f6c5f8246cb46086276e225c57f363
franco giovannangelo <fcgiova@gmail.com>
2020-08-26 12:43:38 -0300

    Updates

---
+++ precache-manifest.24673777b6dfeea5d3c66d42a5e8eef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42df460bf0ef71e62c40b0410f211b84",
+    "url": "/Tic-tac-toe/index.html"
+  },
+  {
+    "revision": "877f356daa999b382ad1",
+    "url": "/Tic-tac-toe/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "5e5e7d6d36bb6553c79c",
+    "url": "/Tic-tac-toe/static/css/main.444b4b23.chunk.css"
+  },
+  {
+    "revision": "877f356daa999b382ad1",
+    "url": "/Tic-tac-toe/static/js/2.c6791239.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-tac-toe/static/js/2.c6791239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5e7d6d36bb6553c79c",
+    "url": "/Tic-tac-toe/static/js/main.6f81e8ea.chunk.js"
+  },
+  {
+    "revision": "b539e28b4d35968c2e31",
+    "url": "/Tic-tac-toe/static/js/runtime-main.bb6c6d65.js"
+  }
+]);
